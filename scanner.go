5cf70200b3ea5965bd023292627184b9eab36bd2
Dylan Finn <dylanfinn@Dylans-MacBook-Pro.local>
2019-02-22 09:39:34 -0800

    gofmt'd

--- scanner.go
+++ scanner.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"net"
 	"sync"
-	"bufio"
-	"errors"
 )
 
 // GetAddrs returns a slice of addresses on the given host
 func GetAddrs(hostname string) ([]string, error) {
 
 	var addresses []string
-	addresses,err := net.LookupHost(hostname)
+	addresses, err := net.LookupHost(hostname)
 
 	if err != nil {
 		return nil, errors.New("Couldn't resolve host")
@@ -21,11 +21,11 @@ func GetAddrs(hostname string) ([]string, error) {
 }
 
 // ConnScan dials host:port addresses and returns a list of successes
-func ConnScan(protocol string, tgthost string, tgtports []int, verb ...bool) (result map[string]string){
+func ConnScan(protocol string, tgthost string, tgtports []int, verb ...bool) (result map[string]string) {
 	var wg sync.WaitGroup
-	
+
 	// parallel for loop pattern
-	for _,v := range tgtports {
+	for _, v := range tgtports {
 		wg.Add(1)
 		address := net.JoinHostPort(tgthost, string(v))
 
@@ -33,19 +33,19 @@ func ConnScan(protocol string, tgthost string, tgtports []int, verb ...bool) (re
 			defer wg.Done()
 
 			// attempt to dial
-			connection,err := net.Dial("tcp", address)
-			status,_ := bufio.NewReader(connection).ReadString('\n')
+			connection, err := net.Dial("tcp", address)
+			status, _ := bufio.NewReader(connection).ReadString('\n')
 			defer connection.Close()
-			
+
 			// success
-			if connection != nil  && err == nil {
+			if connection != nil && err == nil {
 				result[address] = status
 
 				// excluded without flag for optimization
 				if verb[1] {
 					fmt.Println("√ ", status)
 				}
-			// fail 	
+				// fail
 			} else {
 				if verb[1] {
 					fmt.Println("X", status)
