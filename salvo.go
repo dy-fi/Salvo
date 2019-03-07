d894d1323ddf23caedc9b4ed303599690461cf01
Dylan Finn <dylanfinn@Dylans-MacBook-Pro.local>
2019-03-07 13:59:05 -0800

    rewrote main script

--- salvo.go
+++ salvo.go
@@ -1,19 +1,10 @@
-package main
+package salvo
 
 import (
 	"fmt"
-	"github.com/dy-fi/salvo"
 	"net"
 	"os"
 )
-
-func _getlist(n int) []int {
-	var result []int
-	for i := 1; i <= n; i++ {
-		result = append(result, i)
-	}
-	return result
-}
 
 func main() {
 	// environment data
@@ -21,31 +12,26 @@ func main() {
 	if err != nil {
 		return
 	}
+
 	fmt.Println(host)
+
 	addrs, err := net.LookupHost(host)
 	if err != nil {
 		fmt.Printf("Error: can't get host \n%v", err)
-		return
 	}
-	fmt.Print(string(len(addrs)))
 
 	var conns map[string]bool
-	ports := _getlist(8000)
 
 	for _, v := range addrs {
-		conns = salvo.PortScan("tcp", v, ports)
+		conns = PortScan("tcp", v, 1000, 3000)
 	}
 
-	if len(conns) == 0 {
-		fmt.Println("No ports were detected")
-	} else {
-		for i, v := range conns {
-			if v {
-				fmt.Println(i + ": success \n")
-			} else {
-				fmt.Println(i + ": failure \n")
-			}
-			
+	for i, v := range conns {
+		if v {
+			fmt.Println(i + ": success \n")
+		} else {
+			fmt.Println(i + ": failure \n")
 		}
 	}
+
 }
